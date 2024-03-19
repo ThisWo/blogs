8e6f63f8918e61d66a9a05cb9d4f4612b54fc2ee
bz <2076773850@qq.com>
2024-03-20 02:58:55 +0800

    第一版

---
+++ .vitepress/config.mjs
@@ -0,0 +1,64 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/image/xiu.svg",
+      },
+    ],
+  ], // head图标
+  title: "2024-龙", // 左上角标题
+
+  description: "奋斗",
+  themeConfig: {
+    // 搜索
+    // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+              closeText: "关闭",
+            },
+          },
+        },
+      },
+    },
+    logo: "/image/龙.svg", // 左上角图标
+    // https://vitepress.dev/reference/default-theme-config
+    // 本地搜索
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "前端", link: "/markdown-examples" },
+    ],
+
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
+    ],
+
+    socialLinks: [{ icon: "github", link: "https://github.com/ThisWo/" }],
+    // 定制底部
+    footer: {
+      copyright: "Copyright © 2024-present My Bai",
+    },
+  },
+});
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "我的前端实验室"
+  text: "哦吼~"
+  tagline: 一直在努力
+  image:
+    src: /image/2024.svg
+    alt: 2024
+  actions:
+    - theme: brand
+      text: 进入百度吗?
+      link: https://www.baidu.com
+    - theme: alt
+      text: 开始
+      link: /api-examples
+
+features:
+  - title: 努力
+    details: 世界在前方 
+  - title: 加油
+    details: 奋斗在路上
+  - title: 成长
+    details: 不断的成长
+---
+
